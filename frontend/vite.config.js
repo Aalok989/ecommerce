b5572f9c6ca5ee455a58be8bd52c20bc0df7dc59
Aalok Kumar <aalok1390@gmail.com>
2024-12-15 09:24:47 +0530

    u

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port: 5173}
-})
+  server: {
+    port: 5173,
+  },
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'UNRESOLVED_IMPORT') {
+          console.warn('Unresolved import:', warning.source);
+        } else {
+          warn(warning);
+        }
+      },
+    },
+  },
+});
