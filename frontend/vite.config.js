eb7a6987b3b42469849323905135953da8c1e78f
Aalok Kumar <aalok1390@gmail.com>
2024-12-17 08:33:32 +0530

    u

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // Set the base path
   plugins: [react()],
-  server:{port: 5173}
+  server: {
+    port: 5173
+  }
 })
